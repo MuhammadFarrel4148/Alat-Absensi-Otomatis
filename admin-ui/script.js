7fc7ead304ab7ca7fcddffd2b15bc5b9b9cb9d8f
Muhammad Farrel <farrelputrapambudi.2020@gmail.com>
2025-05-02 19:23:34 +0700

    add fetch statistic data

--- admin-ui/script.js
+++ admin-ui/script.js
@@ -1,21 +1,46 @@
 const ctx = document.getElementById('chart');
+const datePicker = document.getElementById('date-picker');
+let currentChart = null;
 
-new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Laki-Laki', 'Perempuan'],
-        datasets: [{
-            label: 'Banyaknya Pengunjung',
-            data: [12, 19],
-            backgroundColor: ['#4A90E2','#FE7743'], 
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
+datePicker.addEventListener('change', async(event) => {
+    const selectedDate = new Date(event.target.value);
+
+    const day = selectedDate.getDate();
+    const month = selectedDate.getMonth() + 1;
+    const year = selectedDate.getFullYear();
+
+    try {
+        const response = await fetch(`http://localhost:3000/statistic?day=${day}&month=${month}&year=${year}`, {
+            method: 'GET'
+        });
+
+        const data = await response.json();
+        if(currentChart) {
+            currentChart.destroy();
+        };
+
+        currentChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ['Laki-Laki', 'Perempuan'],
+                datasets: [{
+                    label: 'Banyaknya Pengunjung',
+                    data: [data.pengunjung_pria, data.pengunjung_wanita],
+                    backgroundColor: ['#4A90E2','#FE7743'], 
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
             }
-        }
-    }
+        });
+
+    } catch(error) {
+
+    };
 });
+
